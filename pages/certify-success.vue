ac891b838ffedf4e9b7c7a17acdd1adbaaeb5694
Saltro <2373117747@qq.com>
2023-04-20 19:37:15 +0800

    feat: 认证成功界面改为回调

--- pages/certify-success.vue
+++ pages/certify-success.vue
@@ -1,6 +1,51 @@
 <template>
-  <page-header :image="image" title="学生身份认证成功" notice="你可以回到应用中继续注册流程" title-gap="15px" />
+  <page-header v-if="isLoading" title="正在认证学生身份" notice="请耐心等待，这个时间不会持续太长" />
+  <page-header
+    v-if="isSuccess"
+    :image="image"
+    title="学生身份认证成功"
+    notice="你可以回到应用中继续注册流程"
+    title-gap="15px"
+  />
+  <page-header v-else-if="!isLoading" title="学生身份认证失败" notice="也许链接已经过期，你可以回到应用中重新发送邮件" />
 </template>
 <script lang="ts" setup>
+import MMessage from 'vue-m-message';
+
+const route = useRoute();
+const code = route.query.code?.toString() ?? '';
+
+const isSuccess = ref(false);
+const isLoading = ref(true);
+
+interface CertifyRes {
+  is_certified: boolean;
+  certify_time: string;
+}
+let restRetry = 3;
+function certify() {
+  if (code.length === 0 || restRetry === 0) {
+    isLoading.value = false;
+    return;
+  }
+  $fetch<ResBody<CertifyRes>>(`/api/users/verify_callback/?code=${code}`, {
+    method: 'GET',
+  })
+    .then((res) => {
+      isSuccess.value = res.data.is_certified;
+    })
+    .catch((err) => {
+      MMessage.error(`${err.data.msg} 正在重试`);
+      setTimeout(() => {
+        restRetry -= 1;
+        certify();
+      }, 500);
+    });
+}
+
+onMounted(() => {
+  certify();
+});
+
 const image = isDark ? '/image/certify-success-dark.svg' : '/image/certify-success.svg';
 </script>
